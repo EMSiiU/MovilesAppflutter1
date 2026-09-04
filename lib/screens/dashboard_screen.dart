6ee49178b9661b16af68f244c29c77d44a189d2d
Emilio Zuñiga <emizc2004@gmail.com>
2026-09-04 10:18:33 -0600

    ADD: Clase moviles | dia 4-09-2026 | menu lateral en dashboard_screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -6,8 +6,46 @@ class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Image.asset('assets/lamar-franklin.gif'),
+      appBar: AppBar(
+        leading: Container(), //quitar el volver default
+      ),
+      endDrawer:Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Emilio Zuñiga"), 
+              accountEmail: Text("Emilio@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/avatarCJ.jpg'), //Imagen local
+                //backgroundImage:NetworkImage("https://i.pravatar.cc/300") //imagen de internet
+              ),  
+            ),
+            ListTile(
+              title: Text('Practica 1 (titulo)'),
+              subtitle: Text("App de práctica (subtitulo)"),
+              leading: Icon(Icons.check),
+              trailing: Icon(Icons.chevron_right),
+            ),
+            ListTile(
+              title: Text('Cambiar tema'),
+              subtitle: Text("Claro / Oscuro"),
+              leading: Icon(Icons.light_mode_outlined),
+              trailing: Icon(Icons.chevron_right),
+            ),
+            ListTile(
+              title: Text('Cerrar sesión'),
+              subtitle: Text("Salir del sistema"),
+              leading: Icon(Icons.logout),
+              trailing: Icon(Icons.chevron_right),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, "/");
+              }
+            )
+          ],
+        ),
+      ),
+
+      //body: Image.asset('assets/lamar-franklin.gif'),
     );
   }
 }
